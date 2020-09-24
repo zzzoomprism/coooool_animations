e996744b47945fc5f7794842780f50593ce18845
zzzoomprism <s96k92@gmail.com>
2020-09-24 13:38:47 +0300

    Finish the all tasks

--- src/components/Pages/Animation/Animation.tsx
+++ src/components/Pages/Animation/Animation.tsx
@@ -3,35 +3,38 @@ import Person from "./Person";
 import { CSSTransition } from "react-transition-group";
 import Help from "./Help";
 
-const Animation = () => {
+const Animation = React.memo(() => {
   let a = 0;
   const [isGo, setIsGo] = useState<boolean>(false);
   const [backgroundPosition, setBackgroundPosition] = useState(0);
   const [isForward, setIsForward] = useState(true);
-  const [showText, setShowText] = useState({
+  const [showText, setShowText] = useState<any>({
     text1: false,
     text2: false,
     text3: false,
   });
   const handleDPress = (event: any) => {
-    if (event.key === "d" || event.keyCode === 39) {
+    if (event.keyCode === 39) {
       setIsGo(true);
       setIsForward(true);
-      a += 4;
+      if (a <= 180) a += 0.3;
       setBackgroundPosition(a);
-      if (a >= 150) setShowText({ ...showText, text1: true });
+      if (Math.floor(a) === 10) setShowText({ ...showText, text1: true });
+      if (Math.floor(a) === 80)
+        setShowText({ ...showText, text1: true, text2: true });
+      if (Math.floor(a) === 100)
+        setShowText({ ...showText, text1: true, text2: true, text3: true });
     }
-    if (event.key === "a" || event.keyCode === 37) {
+
+    if (event.keyCode === 37) {
       setIsGo(true);
       setIsForward(false);
-      a -= 4;
+      if (a >= 10) a -= 0.3;
       setBackgroundPosition(a);
     }
   };
   const handleDUp = (event: any) => {
-    if (event.key === "d") {
-      setIsGo(false);
-    } else setIsGo(false);
+    setIsGo(false);
   };
   useEffect(() => {
     document.addEventListener("keydown", handleDPress, false);
@@ -41,55 +44,81 @@ const Animation = () => {
       document.removeEventListener("keyup", handleDUp, false);
     };
   }, []);
+  let background_options = [
+    {
+      style: { left: `calc(0% - ${backgroundPosition}%` },
+      text: (
+        <div
+          className={"background_animation_item__text"}
+          style={{ left: `calc(30% - ${backgroundPosition}%` }}
+        >
+          <h3 className="mt-3">Hello Guys!</h3>
+          <p>made by Aliaksandra Susha</p>
+          <p> Technologies : React, SCSS, React-transition-group</p>
+        </div>
+      ),
+    },
+    {
+      style: {
+        left: `calc(100% - ${backgroundPosition}%`,
+        transform: "scale(-1, 1)",
+      },
+      text: (
+        <div
+          className={"background_animation_item__text"}
+          style={{ left: `calc(120% - ${backgroundPosition}%` }}
+        >
+          <h3 className="mt-3">Hello Guys!</h3>
+          <p>made by Aliaksandra Susha</p>
+          <p> Technologies : React, SCSS, React-transition-group</p>
+        </div>
+      ),
+    },
+    {
+      style: { left: `calc(200% - ${backgroundPosition}%` },
+      text: (
+        <div
+          className={"background_animation_item__text"}
+          style={{ left: `calc(190% - ${backgroundPosition}%` }}
+        >
+          <h3 className="mt-3">Hello Guys!</h3>
+          <p>made by Aliaksandra Susha</p>
+          <p> Technologies : React, SCSS, React-transition-group</p>
+        </div>
+      ),
+    },
+  ];
 
+  let background_div = background_options.map((el, index) => {
+    console.log(index);
+    return (
+      <div className="background_animation_item">
+        <img
+          src="https://i.pinimg.com/originals/78/81/2f/78812fd262025d24e53452a1307bbb6d.png"
+          className="background_image"
+          alt="background"
+          style={el.style}
+        />
+        <CSSTransition
+          in={showText[`text${index + 1}`]}
+          timeout={1000}
+          classNames="my-node"
+          unmountOnExit
+          appear
+        >
+          {el.text}
+        </CSSTransition>
+      </div>
+    );
+  });
   return (
     <div className="animation__block">
-      <Help />
-      <div className="background_animation">
-        <div className="background_animation_item">
-          <img
-            src="https://i.pinimg.com/originals/78/81/2f/78812fd262025d24e53452a1307bbb6d.png"
-            className="background_image"
-            alt="background"
-            style={{ left: `calc(0% - ${backgroundPosition}px` }}
-          />
-          <CSSTransition
-            in={showText.text1}
-            timeout={1000}
-            classNames="my-node"
-            unmountOnExit
-            appear
-          >
-            <div
-              className={"background_animation_item__text"}
-              style={{ left: `calc(30% - ${backgroundPosition}px` }}
-            >
-              <h3 className="mt-3">Hello Guys!</h3>
-              <p>made by Aliaksandra Susha</p>
-              <p> Technologies : React, SCSS, React-transition-group</p>
-            </div>
-          </CSSTransition>
-        </div>
-
-        <img
-          src="https://i.pinimg.com/originals/78/81/2f/78812fd262025d24e53452a1307bbb6d.png"
-          className="background_image img2"
-          alt="background"
-          style={{
-            left: `calc(100% - ${backgroundPosition}px`,
-            transform: "scale(-1, 1)",
-          }}
-        />
-        <img
-          src="https://i.pinimg.com/originals/78/81/2f/78812fd262025d24e53452a1307bbb6d.png"
-          className="background_image img3"
-          alt="background"
-          style={{ left: `calc(200% - ${backgroundPosition}px` }}
-        />
-      </div>
+      {console.log(backgroundPosition)}
+      <Help direction={isForward} isGo={isGo} />
+      <div className="background_animation">{background_div}</div>
       <Person isGo={isGo} isForward={isForward} />
     </div>
   );
-};
+});
 
 export default Animation;
--- src/components/Pages/Animation/Help.tsx
+++ src/components/Pages/Animation/Help.tsx
@@ -1,7 +1,43 @@
 import React from "react";
 
-const Help = () => {
-  return <h4 className="little_help">Use your arrows or WASD :)</h4>;
+type PropsType = {
+  direction: boolean;
+  isGo: boolean;
 };
+
+const Help: React.FC<PropsType> = React.memo(({ direction, isGo }) => {
+  console.log(direction);
+  return (
+    <div>
+      <h4 className="little_help">Use your arrows or WASD :)</h4>;
+      <div className="arrow_btns">
+        <span className="arrow_top_btn">
+          <i className="fas fa-long-arrow-alt-up"></i>
+        </span>
+        <span
+          className="arrow_right_btn"
+          style={{
+            backgroundColor: `${direction && isGo ? "#faf16db2" : "#f1f1f16b"}`,
+          }}
+        >
+          <i className="fas fa-long-arrow-alt-right"></i>
+        </span>
+        <span className="arrow_bottom_btn">
+          <i className="fas fa-long-arrow-alt-down"></i>
+        </span>
+        <span
+          className="arrow_left_btn"
+          style={{
+            backgroundColor: `${
+              !direction && isGo ? "#faf16db2" : "#f1f1f16b"
+            }`,
+          }}
+        >
+          <i className="fas fa-long-arrow-alt-left"></i>
+        </span>
+      </div>
+    </div>
+  );
+});
 
 export default Help;
